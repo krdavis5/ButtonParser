74298c75e27c4962abb5cfb5614a53a032c058bd
Trey Davis <treydavis@outlook.com>
2017-01-26 08:17:49 -0600

    Fixed directory structure

---
+++ LinkButtonizer.i18n.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @since 0.1.0
+ *
+ * @file
+ *
+ * @licence GNU GPL
+ * @author Trey Davis (krdavis5)
+ */
+
+$magicWords = array();
+
+/** English
+ * @author Your Name (YourUserName)
+ */
+$magicWords['en'] = array(
+   'button' => array( 0, 'button' ),
+);
